ef4503e42b4a7d11c6737b4fe647a1c2e50d2db0
JosephRArsany <jrsany2903@gmail.com>
2025-10-15 19:11:35 +0100

    single looping functionality works, now solve nested looping

--- sln.py
+++ sln.py
@@ -1,26 +1,17 @@
 a = "clear x; clear y; incr x; incr x; incr x; decr x; incr y; incr y; incr y; incr y; incr y; clear z; while y not 0 do; incr z; decr y; end 0;"#extra 0 on end to match format
 b = "clear x; clear y; incr x; incr x; incr x; decr x; incr y; incr y; incr y; incr y; incr y; clear z;"
-g = {}
+nestedtest = "clear x; clear y; incr x; incr x; incr x; decr x; incr y; incr y; incr y; incr y; incr y; clear z; while y not 0 do; incr z; decr y; while x not 0 do; decr x; incr y; end 0; decr y; end 0;"
+g = {"__CANEXEC__": True}
 stack = []
-f = {"clear":lambda x,i: ((g.__setitem__(x, 0), (0,0))[1]),
-     "incr":lambda x,i: ((g.__setitem__(x, g.get(x) + 1)), (0,0))[1],   
-     "decr":lambda x,i: ((g.__setitem__(x, g.get(x) - 1)), (0,0))[1],
-     "while":lambda x,i: ((stack.insert(0, (x,i)) if g.get(x) != 0 else None), (0,0), print(stack))[1],
-     "end":lambda x,i: (print(stack, i, "ss"),(stack.pop(0)[1],i + 1))[1]}
-#main = lambda b:list(map(lambda x: f[x[1][0]](x[1][1], x[0]),enumerate(filter(lambda x: x != [''] ,map(lambda x: x.split(" "),map(lambda x: x.strip(), b.split(";")))))))
-#
-#test = lambda c: list(map(lambda x: (f[x[1][0]](x[1][1], x[0]), print(x[1][0]))[0], enumerate(filter(lambda x: x != [''] ,map(lambda x: x.split(" "),map(lambda x: x.strip(), c.split(";")))))))
+f = {"clear":lambda x,i: ((g.__setitem__(x, 0) if g["__CANEXEC__"] else None, (0,0))[1]),
+     "incr":lambda x,i: ((g.__setitem__(x, g.get(x) + 1)) if g["__CANEXEC__"] else None, (0,0))[1],   
+     "decr":lambda x,i: ((g.__setitem__(x, g.get(x) - 1)) if g["__CANEXEC__"] else None, (0,0))[1],
+     "while":lambda x,i: ((stack.insert(0, (x,i)) if g.get(x) != 0 else g.__setitem__("__CANEXEC__", False)), (0,0))[1],
+     "end":lambda x,i: ((stack.pop(0)[1],i + 1) if g["__CANEXEC__"] else (0,0), g.__setitem__("__CANEXEC__", True))[0]}
 
-#test(a)
+main = lambda c: map(lambda y: list(map(lambda m: list(main(m)), ["".join(list(map(lambda x: (" ".join(x) + ";") ,list(filter(lambda x: x != [''] ,map(lambda x: x.split(" "),map(lambda x: x.strip(), c.split(";")))) ) [y[0]:y[1]])))]  )), list(map(lambda x: (print(g, stack, x),f[x[1][0]](x[1][1], x[0]))[1], enumerate(filter(lambda x: x != [''] ,  map(lambda x: x.split(" "), map(lambda x: x.strip(), c.split(";")))  )))))
+list(main(a))
 
-main = lambda c: map(lambda y: list(map(lambda m: (print(m, "safas"),main(m))[0], list(map(lambda x: (" ".join(x) + ";") ,list(filter(lambda x: x != [''] ,map(lambda x: x.split(" "),map(lambda x: x.strip(), c.split(";")))) ) [y[0]:y[1]]))))       ,list(map(lambda x: (print(stack, x, "sds"),f[x[1][0]](x[1][1], x[0]))[1], enumerate(filter(lambda x: x != [''] ,map(lambda x: x.split(" "),map(lambda x: x.strip(), c.split(";"))))))))
-#### Almost works, however the m passed in the lambda funtion here is individual lines not full loop block
+print("Final result:\n", g)
 
-
-list(main(a))    
-#print(*main(a))
-#main = lambda b:list(map(lambda x: f[x[0]](x[1]),filter(lambda x: x != [''] ,map(lambda x: x.split(" "),map(lambda x: x.strip(), b.split(";"))))))
-print(stack)
-print(g)
-
-###notes to finish, enumerate and pass index into f as parameter, save index on stack, recursivly call main on added commands
+#nested loops reach infinite recussion, need to fix
